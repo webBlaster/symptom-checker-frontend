c8df3fce3d23dada03b56e00ed23b1e619d8dd9a
Yekinni Ajibola <yekinnijibola@gmail.com>
2022-01-31 21:18:38 +0100

    feature: added round add buttons

--- src/components/result.js
+++ src/components/result.js
@@ -6,24 +6,25 @@ const Result = ({ result, addSymptom }) => {
       result.map((res) => {
         return (
           <li key={Math.random() * 100}>
-            {res._source.Name}{" "}
+            {res._source.Name} <br />
             <span>
               {" "}
               <button
                 onClick={() => {
                   addSymptom(res._source);
                 }}
+                class="btn-floating btn-small waves-effect waves-light green"
               >
-                Add
+                <i class="material-icons">add</i>
               </button>
             </span>
           </li>
         );
       })
     ) : (
-      <p></p>
+      <p>No symptoms added</p>
     );
-  return <ul>{response}</ul>;
+  return <ul className="symptoms-container">{response}</ul>;
 };
 
 export default Result;
